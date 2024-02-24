5dd24956608e393a5a4ad09f52084cb7b5a22a59
Lorraxs <tringuyenk19@gmail.com>
2024-02-24 09:50:25 +0700

    change icon props

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,14 +1,58 @@
 import React from 'react';
 import Box, { BoxProps } from './Box';
+import useReponsiveProps from '../hooks/useReponsiveProps';
 export type IconProps = {
-  icon: React.ReactNode;
+  icon: React.ReactNode | string;
 } & BoxProps;
 
 function Icon(props: IconProps) {
-  const { icon, ...rest } = props;
-
+  const {
+    children,
+    onClick,
+    onBlur,
+    onMouseEnter,
+    onMouseLeave,
+    onMouseOver,
+    onContextMenu,
+    icon,
+    ...rest
+  } = props;
+  const ratioStyle = useReponsiveProps(props);
+  if (typeof icon === 'string') {
+    return (
+      <i
+        className={`icon-${icon}`}
+        style={{
+          ...ratioStyle,
+          ...rest,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        onClick={onClick}
+        onBlur={onBlur}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        onMouseOver={onMouseOver}
+        onContextMenu={onContextMenu}
+      >
+        {children}
+      </i>
+    );
+  }
   return (
-    <Box {...rest} display="flex" justifyContent="center" alignItems="center ">
+    <Box
+      {...rest}
+      display="flex"
+      justifyContent="center"
+      alignItems="center "
+      onClick={onClick}
+      onBlur={onBlur}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      onMouseOver={onMouseOver}
+      onContextMenu={onContextMenu}
+    >
       {icon}
     </Box>
   );
